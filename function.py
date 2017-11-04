6e1b6b3ea66062143dac887d3b2050c0be2edfa3
Chethan U C007 <evolutionmanager123@gmail.com>
2017-11-04 23:47:01 +0530

    Final Build.

---
+++ function.py
@@ -0,0 +1,20 @@
+from utils import *
+
+def reduce_puzzle(values):
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values) 
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with  zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+             return False
+    return values
